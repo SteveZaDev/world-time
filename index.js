714d62da5de448e5902050f3b5b2ca2060640718
SteveZaDev <118739097+SteveZaDev@users.noreply.github.com>
2023-05-27 09:36:01 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -158,6 +158,11 @@ function updateClock() {
     ampm = "PM";
   } 
 
+  if(h === 0){
+    h = 12;
+    ampm = "AM";
+  }
+
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
